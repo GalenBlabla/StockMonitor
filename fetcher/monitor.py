c1928abec8b0e72558ddc7c518dcfe9a2fef699a
hailong <hailong@ecut.edu.cn>
2023-03-12 20:29:04 +0800

    完善了股票更新机制

--- fetcher/monitor.py
+++ fetcher/monitor.py
@@ -13,7 +13,11 @@ class StockCountFetcher:
     """
     __API_SH_Stock = "https://gushitong.baidu.com/opendata"
     __API_SZ_Stock = "https://gushitong.baidu.com/opendata"
-    __HEADERS = {""}
+    __HEADERS = {
+        "Cookie": "BIDUPSID=4154BAE7E88A3C256C441776949A2B1F; PSTM=1632890871; __yjs_duid=1_f47d839a61cba4039e210bbec74edfc11632975760217; BAIDUID=5C09C8E4512FE4E5AB1338D54340F9F6:FG=1; MCITY=-163%3A; newlogin=1; MAWEBCUID=web_dWKLuQjvniuSwIfDUmeJWpoSJitqQTaQMGdHXiBblvOZOuGHbB; BDUSS=k9iN3VoV35iMjFDd0pXYklrVWNTZ1ZWbjRBMlFCMFlTdTRPM0pxMHhGZTFqQzVrSVFBQUFBJCQAAAAAAQAAAAEAAADjzYZAVHJhc2hlc3RBcHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALX~BmS1~wZkd; BDUSS_BFESS=k9iN3VoV35iMjFDd0pXYklrVWNTZ1ZWbjRBMlFCMFlTdTRPM0pxMHhGZTFqQzVrSVFBQUFBJCQAAAAAAQAAAAEAAADjzYZAVHJhc2hlc3RBcHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAALX~BmS1~wZkd; ab_sr=1.0.1_NDgzNzQ4YzNjMWQyN2FlNjU2NWM4ODU0OTM0YjRmY2NjYjE1M2UzNTE1M2FjYjMwMGY1YTMzZDlkODZhN2IzYWQ5MGM4M2U4MmY5M2ZkZmRmZjkxOWMwZDhhYmM2ZTFmZmU2OWI3NTY2NGU5MzQyMGIwNjYyZDRkYTQ2YjAwMTM3ZjA3ZWYxOGVjZWU3NWFkMzk1MmNkYWU1NmIxYWI1OA==; BA_HECTOR=0501842284a5818g0h20a58b1i0e4861m; ZFY=vD0w8lrD39F2mrHjQEwBY7bmvW5pxRHy06U:AmCaFQZY:C; BAIDUID_BFESS=5C09C8E4512FE4E5AB1338D54340F9F6:FG=1; PSINO=3; delPer=0; H_PS_PSSID=37779_36542_37554_38123_37906_38170_38289_38236_36802_38261_37934_38313_38322_37900_26350_38282_37881",
+        "Accept-Encoding": "gzip, deflate, br",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31"
+    }
 
     def __init__(self):
         pass
@@ -117,18 +121,26 @@ class StockInfoSyncFetcher:
         if response.status_code == 200:
             stocks_data = response.json()
             stocks_to_create = []
+            new_stocks = []  # 用于存储新添加的股票信息
+            codes_set = set(await StockList.all().values_list('stock_code', flat=True))
             for stock in stocks_data:
-                stocks_to_create.append(
-                    StockList(
+                if stock['dm'] not in codes_set:
+                    stock_obj = StockList(
                         stock_code=stock['dm'],
                         name=stock['mc'],
                         exchange=stock['jys']
                     )
-                )
+                    stocks_to_create.append(stock_obj)
+                    new_stocks.append(f"{stock_obj.name}({stock_obj.stock_code}) - {stock_obj.exchange}")
+                    codes_set.add(stock['dm'])
             await StockList.bulk_create(stocks_to_create)
             print(f'Saved {len(stocks_to_create)} stocks to database.')
+            return new_stocks  # 返回新添加股票的信息列表
         else:
             print(f'Failed to fetch stock data. Status code: {response.status_code}')
+            return []
+
+
 #
 # if __name__ == '__main__':
 #     a = StockCountFetcher()
